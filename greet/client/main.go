a5131569326604b33a84cb37baed87beb3b61eb9
nandonyata <nandonp02@gmail.com>
2024-01-23 14:15:23 +0700

    feat: init client

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var address string = "localhost:3001"
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed connection client: %v\n", err)
+	}
+
+	defer conn.Close()
 }
